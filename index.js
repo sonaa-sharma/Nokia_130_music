798ef6a38d053da58e2a59f9a85ee4443618fc89
Sonam Sharma <aspirantss123@gmail.com>
2024-03-03 17:24:46 +0530

    animation added

--- index.js
+++ index.js
@@ -1,18 +1,90 @@
 var powerButton = document.getElementById("power-button");
 
 var timeoutId;
-var isDeviceOn = false
+var isDeviceOn = false;
+var brandAnimation = document.getElementById("brand-animation");
+brandAnimation.addEventListener("ended", onBrandAnimationFinish);
 
+function showBrandAnimation() {
+  displayBrandAnimation(false);
+  brandAnimation.play();
+}
+
+function displayBrandAnimation(show) {
+  var animationContainer = document.getElementById("brand-animation-screen");
+
+  if (show === false) {
+    animationContainer.classList.remove("hide");
+  } else {
+    animationContainer.classList.add("hide");
+  }
+}
+
+function showLockScreen() {
+  displayWallPaper();
+  displayNavbar();
+  displayDateTime();
+}
+
+function getNavbar() {
+  return document.getElementById("navbar");
+}
+function displayNavbar(show) {
+  var navbar = getNavbar();
+
+  if (show === false) {
+      navbar.classList.add("hide");
+    } else {
+      navbar.classList.remove("hide");
+  }
+}
+
+function displayDateTime() {}
+
+function displayWallPaper(show) {
+  var lcd = getLcd();
+
+  if (show === false) {
+    lcd.classList.remove("lcd-on");
+  } else {
+    lcd.classList.add("lcd-on");
+  }
+}
+
+function displayWhiteScreen(show) {
+  var lcd = getLcd();
+
+  if (show === false) {
+    lcd.classList.remove("lcd-white");
+  } else {
+    lcd.classList.add("lcd-white");
+  }
+}
+
+function displayBlackScreen(show) {
+  var lcd = getLcd();
+
+  if (show === false) {
+    lcd.classList.remove("lcd-off");
+  } else {
+    lcd.classList.add("lcd-off");
+  }
+}
+
+function onBrandAnimationFinish() {
+  displayBrandAnimation();
+  displayWhiteScreen(false);
+  showLockScreen();
+}
 
 function handlePowerOn() {
-    if(isDeviceOn){
-        turnOfflcd();
-    }
-    else{
-        turnOnlcd();
-    }
+  if (isDeviceOn) {
+    turnOfflcd();
+  } else {
+    turnOnlcd();
+  }
 
-    isDeviceOn = !isDeviceOn
+  isDeviceOn = !isDeviceOn;
 }
 
 function startPoweringOn(event) {
@@ -22,27 +94,23 @@ function startPoweringOn(event) {
 function stopPoweringOn(event) {
   clearTimeout(timeoutId);
 }
-function getLcd(){
-    return document.getElementById("lcd");
+function getLcd() {
+  return document.getElementById("lcd");
 }
 
-function turnOnlcd(){
-     var lcd = getLcd();
-    if(lcd.classList.contains("lcd-off")){
-        lcd.classList.remove('lcd-off');
-    }
-    
-    lcd.classList.add("lcd-white");
-    
+function turnOnlcd() {
+  var lcd = getLcd();
+  displayBlackScreen(false);
+  displayWhiteScreen();
+  showBrandAnimation();
 }
 
-function turnOfflcd(){
-    var lcd = getLcd();
-    if(lcd.classList.contains("lcd-white")){
-        lcd.classList.remove('lcd-white');
-    }
-        lcd.classList.add("lcd-off");
-
+function turnOfflcd() {
+  var lcd = getLcd();
+  displayWhiteScreen(false);
+  displayWallPaper(false);
+  displayNavbar(false);
+  displayBlackScreen();
 }
 
 powerButton.addEventListener("mousedown", startPoweringOn);
