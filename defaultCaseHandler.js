5af3be47fa1f30d0a741e83d85e83042138676b7
Sonam Sharma <aspirantss123@gmail.com>
2024-04-24 10:51:28 +0530

    codes refactored

--- defaultCaseHandler.js
+++ defaultCaseHandler.js
@@ -1,29 +1,27 @@
-function defaultCaseHandler(button){
-    switch(button.id){
-      case "right-select-button":
-        mountDefaultMessage(false);
-        showMenu(false);
-        break;
-  
-      case "power-button":
-        mountDefaultMessage(false);
-        showMenu(true);
-        break;
-  
-      default:
-        break;
-    }
-  }
-  
-  function showDefaultMessage(){
-    mountDefaultMessage(true);
-    screenName = "messageScreen";
-  }
+function defaultCaseHandler(button) {
+  switch (button.id) {
+    case "right-select-button":
+      mountDefaultMessage(false);
+      showMenu(false);
+      break;
 
-  function mountDefaultMessage(show){
-    var music = document.getElementById("message-container");
-    AddRemoveClassList(music, "hide", !show);
+    case "power-button":
+      mountDefaultMessage(false);
+      showMenu(true);
+      break;
+
+    default:
+      break;
   }
-  
-  
-  
+}
+
+function showDefaultMessage() {
+  mountIdleScreenWallPaper(true);
+  mountDefaultMessage(true);
+  screenName = "messageScreen";
+}
+
+function mountDefaultMessage(show) {
+  var music = document.getElementById("message-container");
+  AddRemoveClassList(music, "hide", !show);
+}
