89e45f97be379e45ee2077bd3437e5e2b4ba3a57
Sonam Sharma <aspirantss123@gmail.com>
2024-05-09 02:22:10 +0530

    video more setting added

--- videoPlayerHandler.js
+++ videoPlayerHandler.js
@@ -1,29 +1,42 @@
 var videoflag = 0;
+var videoTimeoutId;
+var videoIndex = 0;
+
+var videoArray = [
+  "videoFolder/butterfly-flowers.mp4", 
+  "videoFolder/butterfly.mp4", 
+  "videoFolder/flowers.mp4",
+  "videoFolder/roses.mp4",
+  "videoFolder/butterfly1.mp4",
+  "videoFolder/squirrel.mp4",
+];
 
 function playVideo() {
-  var videoPlayer = document.getElementById("videoPlayerId");
+  var videoPlayer = document.getElementById("videoId");
   var playPause = document.getElementById("play-pause-id");
-  playPause.src = "Icons/pause-play.png"
+  playPause.src = "Icons/pause-play.png";
   videoPlayer.play();
+  videoTimeoutId = setInterval(moveProgressBar, 500);
+  videoPlayer.addEventListener("ended", onVideoEnd);
   videoflag = 1;
 }
 
+function onVideoEnd() {
+  pauseVideo();
+}
+
 function pauseVideo() {
-  var videoPlayer = document.getElementById("videoPlayerId");
+  var videoPlayer = document.getElementById("videoId");
   var playPause = document.getElementById("play-pause-id");
   videoPlayer.pause();
-  playPause.src = "Icons/play (1).png"
+  clearInterval(videoTimeoutId);
+  playPause.src = "Icons/play (1).png";
 }
 
 function videoPlayerHandler(button) {
   switch (button.id) {
     case "left-select-button":
-      // if (videoflag === 1) {
-      //   pauseVideo();
-      //   videoflag = 0;
-      // } else {
-      //   playVideo();
-      // }
+    
       break;
 
     case "mid-button-inner":
@@ -32,6 +45,8 @@ function videoPlayerHandler(button) {
         videoflag = 0;
       } else {
         playVideo();
+      // showNextVideo();
+
       }
       break;
 
@@ -45,26 +60,131 @@ function videoPlayerHandler(button) {
       showIdleScreen();
       break;
 
+    case "right-button":
+      showNextVideo();
+      break;
+
+    case "left-button":
+      showPreviousVideo();
+      break;
+
     default:
       break;
   }
 }
 
+function showNextVideo(){
+  var playPause = document.getElementById("play-pause-id");
+  playPause.src = "Icons/play (1).png";
+  clearInterval(videoTimeoutId);
+  var barNode = document.getElementById("progress-bar-id");
+  barNode.value = 0;
+  videoflag = 0;
+
+  showSelection("right-button");
+
+  if(videoIndex === videoArray.length){
+    videoIndex = 0;
+  }
+  var videoNode = videoArray[videoIndex];
+  var videoBoxId = document.getElementById("videoId");
+  videoBoxId.src = videoNode;
+  console.log(videoNode);
+  videoIndex++;
+}
+
+function showPreviousVideo(){
+  var playPause = document.getElementById("play-pause-id");
+  playPause.src = "Icons/play (1).png";
+  clearInterval(videoTimeoutId);
+  var barNode = document.getElementById("progress-bar-id");
+  barNode.value = 0;
+  videoflag = 0;
+
+  showSelection("left-button");
+
+  if(videoIndex < 0){
+    videoIndex = videoArray.length-1;
+  }
+  var videoNode = videoArray[videoIndex];
+  var videoBoxId = document.getElementById("videoId");
+  videoBoxId.src = videoNode;
+  console.log(videoIndex);
+  console.log(videoNode);
+  videoIndex--;
+}
+
+function showSelection(idName){
+  if(idName === "left-button"){
+    var buttonClick = document.getElementById(idName);
+    buttonClick.addEventListener("mousedown", showBorderBackwardButton);
+    buttonClick.addEventListener("mouseup", hideBorderBackwardButton);
+  }
+  else{
+    var buttonClick = document.getElementById(idName);
+    buttonClick.addEventListener("mousedown", showBorderForwardButton);
+    buttonClick.addEventListener("mouseup", hideBorderForwardButton);
+  }
+}
+
+function showBorderBackwardButton(){
+  var backwardButton = document.getElementById("previous-video");
+  backwardButton.style.border = "1px solid white";
+
+}
+
+function hideBorderBackwardButton(){
+  var backwardButton = document.getElementById("previous-video");
+  backwardButton.style.border = "1px solid ";
+
+}
+
+function showBorderForwardButton(){
+  var backwardButton = document.getElementById("next-video");
+  backwardButton.style.border = "1px solid white";
+
+}
+
+function hideBorderForwardButton(){
+  var backwardButton = document.getElementById("next-video");
+  backwardButton.style.border = "1px solid ";
+
+}
+
 function showVideoPlayer() {
   mountVideoPlayerScreen(true);
-//   mountNavbar(true);
-  // mountIdleScreenWallPaper(true);
-
   screenName = "videoPlayerScreen";
+  videoIndex = 0;
+  var videoNode = "videoFolder/butterfly1.mp4";
+  var videoBoxId = document.getElementById("videoId");
+  videoBoxId.src = videoNode;
+
 }
 
 function hideVideoPlayer() {
   mountVideoPlayerScreen(false);
-//   mountNavbar(false);
   mountIdleScreenWallPaper(false);
 }
 
 function mountVideoPlayerScreen(show) {
   var video = document.getElementById("video-player-container");
   AddRemoveClassList(video, "hide", !show);
+}
+
+function getVideoPlayedPercentage() {
+  var videoNode = document.getElementById("videoId");
+  var totalTime = videoNode.duration;
+  var currentTime = videoNode.currentTime;
+  var percentage = (currentTime / totalTime) * 100;
+  return Math.ceil(percentage);
+}
+
+function moveProgressBar() {
+  var per = getVideoPlayedPercentage();
+  var barNode = document.getElementById("progress-bar-id");
+  barNode.value = per;
+}
+
+function printTimer() {
+  console.log(getVideoPlayedPercentage());
 }
