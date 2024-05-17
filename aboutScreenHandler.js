729fbd7a86f93a8f3d05791cea679b3d9bc123e7
Sonam Sharma <aspirantss123@gmail.com>
2024-05-17 14:45:34 +0530

    code refactored

--- aboutScreenHandler.js
+++ aboutScreenHandler.js
@@ -1,40 +1,30 @@
-
 function aboutScreenHandler(button) {
-    switch (button.id) {
-      case "left-select-button":
-        
-        break;
-  
-      case "mid-button-inner":
-       
-        break;
-  
-      case "right-select-button":
-        hideAboutScreen();
-        showMenu();
-        break;
-  
-      case "power-button":
-        hideAboutScreen();
-        showIdleScreen();
-        break;
-  
-      default:
-        break;
-    }
+  switch (button.id) {
+    case "right-select-button":
+      hideAboutScreen();
+      showMenu();
+      break;
+
+    case "power-button":
+      hideAboutScreen();
+      showIdleScreen();
+      break;
+
+    default:
+      break;
   }
-  
-  function showAboutScreen() {
-    mountAboutScreen(true);
-    screenName = "aboutScreen";
-  }
-  
-  function hideAboutScreen() {
-    mountAboutScreen(false);
-  }
-  
-  function mountAboutScreen(show) {
-    var music = document.getElementById("about-screen-id");
-    AddRemoveClassList(music, "hide", !show);
-  }
-  
+}
+
+function showAboutScreen() {
+  mountAboutScreen(true);
+  screenName = "aboutScreen";
+}
+
+function hideAboutScreen() {
+  mountAboutScreen(false);
+}
+
+function mountAboutScreen(show) {
+  var aboutScreen = document.getElementById("about-screen-id");
+  AddRemoveClassList(aboutScreen, "hide", !show);
+}
