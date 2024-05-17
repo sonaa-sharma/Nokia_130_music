14cc6a1dafd39cade1880b7a62712a0f8d5f82fc
Sonam Sharma <aspirantss123@gmail.com>
2024-05-17 12:45:22 +0530

    calculation logic added

--- calculatorHandler.js
+++ calculatorHandler.js
@@ -1,17 +1,36 @@
 var calculatorHandlerConfig = null;
 
+const operators = {
+  add: "+",
+  multiply: "*",
+  divide: "/",
+  subtract: "-",
+};
+
 function getInitialCalculatorConfig() {
-  return { firstParam: 0, secondParam: 0 };
+  return {
+    maxValue: 9999999999,
+    firstParam: 0,
+    secondParam: 0,
+    selectedOperator: null,
+    operatorIds: {
+      [operators.add]: "add-signId",
+      [operators.multiply]: "mul-signId",
+      [operators.divide]: "div-signId",
+      [operators.subtract]: "sub-signId",
+    },
+  };
 }
 
 function calculatorHandler(button) {
+  var ids = calculatorHandlerConfig.operatorIds;
   switch (button.id) {
     case "left-select-button":
+      calculateResult();
+      resetValuesToInitial();
       break;
 
     case "mid-button-inner":
-      showEqualOperatorSign();
-      // calculateResult();
       break;
 
     case "right-select-button":
@@ -25,50 +44,50 @@ function calculatorHandler(button) {
       break;
 
     case "top-button":
-      highlightOperator("add-signId");
+      selectOperator(operators.add);
       break;
 
     case "left-button":
-      highlightOperator("mul-signId");
+      selectOperator(operators.multiply);
       break;
 
     case "right-button":
-      highlightOperator("div-signId");
+      selectOperator(operators.divide);
       break;
 
     case "bottom-button":
-      highlightOperator("sub-signId");
+      selectOperator(operators.subtract);
       break;
 
     case "0":
-      updateAndShowFirstParam(0);
+      updateParam(0);
       break;
     case "1":
-      updateAndShowFirstParam(1);
+      updateParam(1);
       break;
     case "2":
-      updateAndShowFirstParam(2);
+      updateParam(2);
       break;
     case "3":
-      updateAndShowFirstParam(3);
+      updateParam(3);
       break;
     case "4":
-      updateAndShowFirstParam(4);
+      updateParam(4);
       break;
     case "5":
-      updateAndShowFirstParam(5);
+      updateParam(5);
       break;
     case "6":
-      updateAndShowFirstParam(6);
+      updateParam(6);
       break;
     case "7":
-      updateAndShowFirstParam(7);
+      updateParam(7);
       break;
     case "8":
-      updateAndShowFirstParam(8);
+      updateParam(8);
       break;
     case "9":
-      updateAndShowFirstParam(9);
+      updateParam(9);
       break;
 
     default:
@@ -80,13 +99,18 @@ function mountCalculator() {
   calculatorHandlerConfig = getInitialCalculatorConfig();
   mountCalculatorScreen(true);
   mountNavbar(true);
-  updateFirstParamInDocument(0);
+  updateParamInDocument(0);
   screenName = "calculatorScreen";
 }
 
 function unmountCalculator() {
   mountCalculatorScreen(false);
   mountNavbar(false);
+  var operator = calculatorHandlerConfig.selectedOperator;
+  if(operator){
+    var operatorId = calculatorHandlerConfig.operatorIds[operator];
+    removeHighlightedOperator(operatorId);
+  }
   calculatorHandlerConfig = null;
 }
 
@@ -95,32 +119,116 @@ function mountCalculatorScreen(show) {
   AddRemoveClassList(calculator, "hide", !show);
 }
 
-function updateFirstParam(input) {
-  if (calculatorHandlerConfig.firstParam > 9999999999) {
-    return calculatorHandlerConfig.firstParam;
+function updateParam(number) {
+  var currentOperator = calculatorHandlerConfig.selectedOperator;
+  if (currentOperator) {
+    var currentId = calculatorHandlerConfig.operatorIds[currentOperator];
+    removeHighlightedOperator(currentId);
+    updateAndShowSecondParam(number);
+    return;
   }
-  return (calculatorHandlerConfig.firstParam =
-    calculatorHandlerConfig.firstParam * 10 + input);
+
+  updateAndShowFirstParam(number);
+}
+
+function updateFirstParam(input) {
+  return (calculatorHandlerConfig.firstParam = addDigitToNumber(
+    input,
+    calculatorHandlerConfig.firstParam
+  ));
 }
 
 function updateAndShowFirstParam(input) {
   var number = updateFirstParam(input);
-  updateFirstParamInDocument(number);
+  updateParamInDocument(number);
 }
 
-function updateFirstParamInDocument(number) {
+function updateParamInDocument(number) {
   var box = document.getElementById("first-inputId");
   box.innerHTML = number;
 }
 
+function updateAndShowSecondParam(input) {
+  var number = updateSecondParam(input);
+  updateParamInDocument(number);
+}
+
+function updateSecondParam(input) {
+  return (calculatorHandlerConfig.secondParam = addDigitToNumber(
+    input,
+    calculatorHandlerConfig.secondParam
+  ));
+}
+
+function addDigitToNumber(digit, number) {
+  if (number > calculatorHandlerConfig.maxValue) {
+    return number;
+  }
+  return number * 10 + digit;
+}
+
 function highlightOperator(operatorSignId) {
   var box = document.getElementById(operatorSignId);
-  box.style.backgroundColor = "orange";
+  AddRemoveClassList(box, "highlight-operator-box", true);
 }
 
 function removeHighlightedOperator(operatorSignId) {
   var box = document.getElementById(operatorSignId);
-  box.style.backgroundColor = "none";
+  AddRemoveClassList(box, "highlight-operator-box", false);
 }
 
-function calculateResult() {}
+function updateValueAfterSelectingOperator(){
+  calculateResult();
+  calculatorHandlerConfig.secondParam = 0;
+}
+
+function selectOperator(operator) {
+  var currentOperator = calculatorHandlerConfig.selectedOperator;
+console.log(currentOperator);
+  if (currentOperator) {
+    var currentId = calculatorHandlerConfig.operatorIds[currentOperator];
+    updateValueAfterSelectingOperator();
+    removeHighlightedOperator(currentId);
+  }
+
+  var nextId = calculatorHandlerConfig.operatorIds[operator];
+  calculatorHandlerConfig.selectedOperator = operator;
+  highlightOperator(nextId);
+}
+
+function calculateResult() {
+  var selectOperator = calculatorHandlerConfig.selectedOperator;
+  if(!selectOperator){
+    return;
+  }
+  var firstParam = calculatorHandlerConfig.firstParam;
+  var secondParam = calculatorHandlerConfig.secondParam;
+  var result;
+  switch (selectOperator) {
+    case operators.add:
+      result = firstParam + secondParam;
+      break;
+    case operators.multiply:
+      result = firstParam * secondParam;
+      break;
+    case operators.divide:
+      result = firstParam / secondParam;
+      break;
+    case operators.subtract:
+      result = firstParam - secondParam;
+      break;
+    default:
+      break;
+  }
+
+  console.log(firstParam);
+  console.log(secondParam);
+  calculatorHandlerConfig.firstParam = result;
+  updateParamInDocument(result);
+}
+
+
+function resetValuesToInitial(){
+  calculatorHandlerConfig.secondParam = 0;
+  calculatorHandlerConfig.selectedOperator = null;
+}
