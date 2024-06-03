73a322c029786f15e8e688252614728797fef26d
Sonam Sharma <aspirantss123@gmail.com>
2024-06-03 15:34:14 +0530

    text anime removed from this branch

--- lockScreenHandler.js
+++ lockScreenHandler.js
@@ -43,26 +43,6 @@ function selectButtonPressed() {
   showButtonPressed("left-select-button", "unlock");
 }
 
-function showButtonPressed(buttonId, elementId) {
-  var buttonClick = document.getElementById(buttonId);
-  buttonClick.addEventListener("mousedown", function () {
-    showShadow(elementId);
-  });
-  buttonClick.addEventListener("mouseup", function () {
-    hideShadow(elementId);
-  });
-}
-
-function showShadow(elementId) {
-  var backwardButton = document.getElementById(elementId);
-  backwardButton.style.boxShadow = "1px 1px 10px white";
-}
-
-function hideShadow(elementId) {
-  var backwardButton = document.getElementById(elementId);
-  backwardButton.style.boxShadow = "none";
-}
-
 function starKeyPressed() {
   if (!isSelectkeyPressed) {
     return;
