70218c5fb41deca9de1aa0f807b7ff62863193d4
sonaa-sharma <142740224+sonaa-sharma@users.noreply.github.com>
2024-06-03 12:07:58 +0530

    keyboard function added (#20)

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ function clickEventFunction(event) {
   buttonClicked(button);
 }
 
-// button click events----------->
-
 function buttonClicked(button) {
-  // console.log(screenName);
   switch (screenName) {
     case "lockScreen":
       lockScreenHandler(button);
@@ -84,8 +81,6 @@ function getButtonNode(node) {
 
   return node;
 }
-
-// phone on-off handlers------------
 
 function setInitialState() {
   initializeEvent();
@@ -213,6 +208,82 @@ function getBrandAnimationNode() {
   return document.getElementById("brand-animation");
 }
 
+function onKeyDown(event) {}
+
+var keyCodeMap = {
+  arrowLeft: 37,
+  arrowUp: 38,
+  arrowRight: 39,
+  arrowDown: 40,
+  enterKey: 13,
+  backSpaceKey: 8,
+  escapeKey: 27,
+  starKey: 106,
+  key0: 48,
+  key1: 49,
+  key2: 50,
+  key3: 51,
+  key4: 52,
+  key5: 53,
+  key6: 54,
+  key7: 55,
+  key8: 56,
+  key9: 57,
+  numPad0: 96,
+  numPad1: 97,
+  numPad2: 98,
+  numPad3: 99,
+  numPad4: 100,
+  numPad5: 101,
+  numPad6: 102,
+  numPad7: 103,
+  numPad8: 104,
+  numPad9: 105,
+};
+
+var keyIdMaping = {
+  [keyCodeMap.arrowLeft]: "left-button",
+  [keyCodeMap.arrowUp]: "top-button",
+  [keyCodeMap.arrowRight]: "right-button",
+  [keyCodeMap.arrowDown]: "bottom-button",
+  [keyCodeMap.enterKey]: "left-select-button",
+  [keyCodeMap.backSpaceKey]: "right-select-button",
+  [keyCodeMap.escapeKey]: "power-button",
+  [keyCodeMap.starKey]: "star-key",
+  [keyCodeMap.key0]: "value-0",
+  [keyCodeMap.key1]: "value-1",
+  [keyCodeMap.key2]: "value-2",
+  [keyCodeMap.key3]: "value-3",
+  [keyCodeMap.key4]: "value-4",
+  [keyCodeMap.key5]: "value-5",
+  [keyCodeMap.key6]: "value-6",
+  [keyCodeMap.key7]: "value-7",
+  [keyCodeMap.key8]: "value-8",
+  [keyCodeMap.key9]: "value-9",
+  [keyCodeMap.numPad0]: "value-0",
+  [keyCodeMap.numPad1]: "value-1",
+  [keyCodeMap.numPad2]: "value-2",
+  [keyCodeMap.numPad3]: "value-3",
+  [keyCodeMap.numPad4]: "value-4",
+  [keyCodeMap.numPad5]: "value-5",
+  [keyCodeMap.numPad6]: "value-6",
+  [keyCodeMap.numPad7]: "value-7",
+  [keyCodeMap.numPad8]: "value-8",
+  [keyCodeMap.numPad9]: "value-9",
+};
+
+function onKeyUp(event) {
+  var id = keyIdMaping[event.keyCode];
+
+  if (!id || !isDeviceOn) {
+    return;
+  }
+
+  var node = document.getElementById(id);
+
+  buttonClicked(node);
+}
+
 function initializeEvent() {
   var keypadButton = document.getElementById("keypad-box");
   var powerButton = document.getElementById("power-button");
@@ -223,6 +294,8 @@ function initializeEvent() {
   powerButton.addEventListener("touchstart", startPoweringOn);
   powerButton.addEventListener("touchend", stopPoweringOn);
   powerButton.addEventListener("touchcancel", stopPoweringOn);
+  document.body.addEventListener("keydown", onKeyDown);
+  document.body.addEventListener("keyup", onKeyUp);
 }
 
 setInitialState();
